9359f561659da2f9687c25e3de95e377df8cc641
Mike <Mhomolak@yahoo.com>
2016-09-08 16:20:39 -0600

    feat: Value listed when slider is moved

--- src/js/component-owner.js
+++ src/js/component-owner.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import {injectIntl} from 'react-intl';
 
 class ComponentOwner extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      value: '50'
+    };
+  }
+
+  updateSliderValue() {
+    this.setState({value: document.getElementById('numInput').value})
   }
 
   render() {
 
     return (
       <span>
-        <label id="slider-label"> Slider placeholder label
-          <input type="range" role="slider" aria-labelledby="slider-label"/>
+        <label id="slider-label">
+        Hidden slider placeholder label
+
+        <input type="range" id="numInput"
+        role="slider"
+        min="0"
+        max="100"
+        value={this.state.value}
+        aria-valuemin="0"
+        aria-valuemax="100"
+        aria-valuenow={this.state.value}
+        onChange={() => this.updateSliderValue()}
+        aria-labelledby="slider-label" tabindex="0" />
+
+        <output>{this.state.value}</output>
         </label>
       </span>
-
     )
   }
 
 }
 
-export default injectIntl(ComponentOwner);
+export default (ComponentOwner);
