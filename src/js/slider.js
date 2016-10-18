94156424701368af330533c1b9c3a99a35e82d5c
Mike <Mhomolak@yahoo.com>
2016-10-18 12:52:35 -0600

    fix: Brought in react-range as a dev dependency to fix IE issues

--- src/js/slider.js
+++ src/js/slider.js
@@ -18,15 +18,18 @@ class SliderComponent extends React.Component {
 
     return (
       <span>
-        <input type="range" id="numInput"
-        min="0"
-        max="100"
-        value={this.state.value}
-        aria-valuemin="0"
-        aria-valuemax="100"
-        aria-valuenow={this.state.value}
-        onChange={(e) => this.updateSliderValue(e)}
-        /> <br/>
+
+        <input
+          type="range"
+          id="numInput"
+          min="0"
+          max="100"
+          value={this.state.value}
+          aria-valuemin="0"
+          aria-valuemax="100"
+          aria-valuenow={this.state.value}
+          onChange={(e) => this.updateSliderValue(e)}
+        /><br/><br/>
 
         <label id="slider-label" HTMLFor="numInput">
         Hidden slider label
